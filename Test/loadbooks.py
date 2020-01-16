c4b010bff12f1029b7a0dc25af47e80473c35ee0
Dario Correal <dcorreal@uniandes.edu.co>
2020-01-16 14:54:18 -0500

    carga de todos los archivos

--- Test/loadbooks.py
+++ Test/loadbooks.py
@@ -22,26 +22,39 @@ print ('Creating tag list')
 lst_tags=  lt.newList()
 print ('Creating books-tag list')
 lst_book_tags= lt.newList()
-
+print ('Creating ratings list')
+lst_ratings= lt.newList()
+print ('Creating to-read list')
+lst_to_read= lt.newList()
 
 print ('Loading books')
 booksfile = cf.data_dir + 'GoodReads/books.csv'
 loadCSVFile (booksfile, lst_books)
 print (lst_books['size'])
-printList (lst_books)
+#printList (lst_books)
 
 
 print ('Loading tags')
 tagsfile = cf.data_dir + 'GoodReads/tags.csv'
 loadCSVFile (tagsfile, lst_tags)
 print (lst_tags['size'])
-printList (lst_tags)
+#printList (lst_tags)
 
 print ('Loading books-tags')
 booktagsfile = cf.data_dir + 'GoodReads/book_tags.csv'
 loadCSVFile (booktagsfile, lst_book_tags)
 print (lst_book_tags['size'])
-printList (lst_book_tags)
+#printList (lst_book_tags)
 
+print ('Loading ratings')
+ratingsfile = cf.data_dir + 'GoodReads/ratings.csv'
+loadCSVFile (ratingsfile, lst_ratings)
+print (lst_ratings['size'])
+#printList (lst_ratings)
 
+print ('Loading books to read')
+toreadfile = cf.data_dir + 'GoodReads/to_read.csv'
+loadCSVFile (toreadfile, lst_to_read)
+print (lst_to_read['size'])
+#printList (lst_to_read)
 
